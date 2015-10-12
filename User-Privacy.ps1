19cbc387e0abb2268032f8cceef53e688513701b
Brad Laue <brad@brad-x.com>
2015-10-12 17:10:13 -0400

    Add conditionals to each registry path to avoid errors.

--- User-Privacy.ps1
+++ User-Privacy.ps1
@@ -3,12 +3,24 @@
 Set-WindowsSearchSetting -EnableWebResultsSetting $false
 
 # We do not accept Microsoft's Privacy Policy
+If (-Not (Test-Path "HKCU:\SOFTWARE\Microsoft\Personalization\Settings")) {
+        New-Item -Force -Path "HKCU:\SOFTWARE\Microsoft\Personalization\Settings" | Out-Null
+}
 New-ItemProperty -Force -Path HKCU:\SOFTWARE\Microsoft\Personalization\Settings -Name AcceptedPrivacyPolicy -Type DWord -Value 0
 # Start Menu: Disable Bing Search Results
+If (-Not (Test-Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search")) {
+        New-Item -Force -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" | Out-Null
+}
 New-ItemProperty -Force -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search -Name BingSearchEnabled -Type DWord -Value 0
 # Do not collect Contact information
+If (-Not (Test-Path "HKCU:\SOFTWARE\Microsoft\InputPersonalization\TrainedDataStore")) {
+        New-Item -Force -Path "HKCU:\SOFTWARE\Microsoft\InputPersonalization\TrainedDataStore" | Out-Null
+}
 New-ItemProperty -Force -Path HKCU:\SOFTWARE\Microsoft\InputPersonalization\TrainedDataStore -Name HarvestContacts -Type DWord -Value 0
 # Do not collect writing and text input data
+If (-Not (Test-Path "HKCU:\SOFTWARE\Microsoft\InputPersonalization")) {
+        New-Item -Force -Path "HKCU:\SOFTWARE\Microsoft\InputPersonalization" | Out-Null
+}
 New-ItemProperty -Force -Path HKCU:\SOFTWARE\Microsoft\InputPersonalization -Name RestrictImplicitInkCollection -Type DWord -Value 1
 New-ItemProperty -Force -Path HKCU:\SOFTWARE\Microsoft\InputPersonalization -Name RestrictImplicitTextCollection -Type DWord -Value 1
 If (-Not (Test-Path "HKCU:\SOFTWARE\Microsoft\Input\TIPC")) {
@@ -20,13 +32,24 @@ New-ItemProperty -Force -Path HKCU:\SOFTWARE\Microsoft\Input\TIPC -Name Enabled 
 #New-ItemProperty -Force -Path "HKCU:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Sensor\Permissions\{BFA794E4-F964-4FDB-90F6-51056BFE4B44}" -Name SensorPermissionState -Type DWord -Value 0
 
 # Disable collection of language data from the environment via the browser
+If (-Not (Test-Path "HKCU:\Control Panel\International\User Profile")) {
+        New-Item -Force -Path "HKCU:\Control Panel\International\User Profile" | Out-Null
+}
 New-ItemProperty -Force -Path "HKCU:\Control Panel\International\User Profile" -Name HttpAcceptLanguageOptOut -Type DWord -Value 1
+
+If (-Not (Test-Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\AppHost")) {
+        New-Item -Force -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\AppHost" | Out-Null
+}
 New-ItemProperty -Force -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\AppHost -Name EnableWebContentEvaluation -Type DWord -Value 0
 
 # Deny Device Access
+If (-Not (Test-Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\DeviceAccess\Global\LooselyCoupled")) {
+        New-Item -Force -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\DeviceAccess\Global\LooselyCoupled" | Out-Null
+}
 New-ItemProperty -Force -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\DeviceAccess\Global\LooselyCoupled -Name Type -Value LooselyCoupled
 New-ItemProperty -Force -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\DeviceAccess\Global\LooselyCoupled -Name Value -Value Deny
 New-ItemProperty -Force -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\DeviceAccess\Global\LooselyCoupled -Name InitialAppValue -Value Unspecified
+
 #foreach ($key in (ls HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\DeviceAccess\Global)) {
 #    if ($key.PSChildName -EQ "LooselyCoupled") {
 #        continue
@@ -36,9 +59,11 @@ New-ItemProperty -Force -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\De
 #    sp (HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\DeviceAccess\Global\ + $key.PSChildName) -Name InitialAppValue -Value Unspecified
 #}
 
-
 ##########
 
+If (-Not (Test-Path "HKCU:\SOFTWARE\Classes\Local Settings\Software\Microsoft\Windows\CurrentVersion\AppContainer\Storage\microsoft.microsoftedge_8wekyb3d8bbwe\MicrosoftEdge")) {
+        New-Item -Force -Path "HKCU:\SOFTWARE\Classes\Local Settings\Software\Microsoft\Windows\CurrentVersion\AppContainer\Storage\microsoft.microsoftedge_8wekyb3d8bbwe\MicrosoftEdge" | Out-Null
+}
 $Edge = "HKCU:\SOFTWARE\Classes\Local Settings\Software\Microsoft\Windows\CurrentVersion\AppContainer\Storage\microsoft.microsoftedge_8wekyb3d8bbwe\MicrosoftEdge"
 
 # 1 adds the Do Not Track Header, 0 does not
